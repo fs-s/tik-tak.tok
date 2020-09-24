b6d66e4031cf5857217852cf3bdc057577b7fed9
fs-s <55434294+fs-s@users.noreply.github.com>
2020-09-24 11:34:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
+let gameBoard = [
+    ['_','_','_'],
+    ['_','_','_'],
+    ['_','_','_']
+]
+
+
 const cellDivs = document.querySelectorAll('.cell');
 
 cellDivs.forEach(element => {
     element.addEventListener('click', event =>{
         const selectedCell = event.currentTarget
+        const selectedRow = selectedCell.dataset.row
+        const selectedCol = selectedCell.dataset.col
+
+
         selectedCell.innerText = 'X'
-        console.log(selectedCell)
+        gameBoard[selectedRow][selectedCol] = 'X'
+        console.log(gameBoard)
+        console.log(selectedCell.dataset.row)
+        console.log(selectedCell.dataset.col)
+
+
     })
 });
 console.log(cellDivs[0].dataset.row)
